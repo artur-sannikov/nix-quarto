b62ed28cb7ca59acf3819d25b213a1514fc6d9f3
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2024-04-12 13:44:33 +0300

    Add: script from building environment with rix

---
+++ build_env.R
@@ -0,0 +1,25 @@
+library(rix)
+
+path_default_nix <- getwd()
+
+rix(r_env = "latest",,
+    git_pkgs = list(
+        list(package_name = "rix",
+        repo_url = "https://github.com/b-rodrigues/rix",
+        branch_name = "master",
+        commit = "76d1bdd03d78589d399b4b9d473ecde616920a82",
+        )
+    ),
+    r_pkgs = c("quarto"),
+    system_pkgs = "quarto",
+    tex_pkgs = c(
+        "amsmath",
+        "orcidlink"
+        "tcolorbox"
+    ),
+    ide = "other",
+    shell_hook = "",
+    project_path = path_default_nix,
+    overwrite = TRUE,
+    print = TRUE
+)
